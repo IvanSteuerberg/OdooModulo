67d4b7a1b2cd2bde4d94b6ee853d7d0b42ee7dd4
ivansteuerberg <irodriguezsteuerberg@danielcastelao.org>
2021-03-24 03:13:16 +0100

    Cambios en la seguridad y permisos

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -7,7 +7,7 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Módulo de Odoo creado por Iván
     """,
 
     'author': "My Company",
@@ -17,14 +17,15 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Education',
-    'version': '0.2',
+    'version': '0.7',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/openacademy.xml',
